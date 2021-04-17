a8222656eb500a13375ae3080f33280ae3c7e113
CastroOlympias <blackcarrera@msn.com>
2021-04-17 16:53:36 -0600

    <am able to now target, the response data from the servie API and offload it into my webpage to spcific element>

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,17 +7,49 @@ var searchTerm = document.querySelector('#city-search').value;
 console.log(searchTerm);
 
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=' +
-searchTerm + 
-'&exclude=hourly,minutely,alerts&appid=0372eaa7cde1ce19de6c28dd0eb2454c')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' +
+    searchTerm + 
+    '&exclude=hourly,minutely,alerts&appid=0372eaa7cde1ce19de6c28dd0eb2454c')
 
-.then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
-  });
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        console.log(response);
+        console.log(response.name);
+
+        var cityDateEl = document.querySelector('#city-date')
+        cityDateEl.innerHTML = '';
+        cityDateEl.textContent = response.main.temp;
+    })
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
 
 }
 
+
+
+
 submitBtn.onclick = findWeather;
+
+        // cityDateEl.innerHTML = '';
+        // cityDateEl.textContent = 'Shit';
+
+    // .catch(function(error) {
+        
+    //     alert('No City Found!');
+    // });
