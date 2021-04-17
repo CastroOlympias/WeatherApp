4149df57be58e124e7f370074fefce7f0d1269b1
CastroOlympias <blackcarrera@msn.com>
2021-04-17 14:26:02 -0600

    <now can search for city and return weather results for that city>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Salt Lake City&exclude=hourly,minutely,alerts&appid=0372eaa7cde1ce19de6c28dd0eb2454c')
+var submitBtn = document.querySelector('#submit-btn');
+
+
+function findWeather(event) {
+event.preventDefault();
+var searchTerm = document.querySelector('#city-search').value;
+console.log(searchTerm);
+
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=' +
+searchTerm + 
+'&exclude=hourly,minutely,alerts&appid=0372eaa7cde1ce19de6c28dd0eb2454c')
 
 .then(function(response) {
     return response.json();
@@ -7,3 +18,6 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Salt Lake City&exclude=
     console.log(data);
   });
 
+}
+
+submitBtn.onclick = findWeather;
