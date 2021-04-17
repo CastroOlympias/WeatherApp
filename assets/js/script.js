879ec878fdf2347dc97d1407e4715ede79ee0723
CastroOlympias <blackcarrera@msn.com>
2021-04-17 11:03:06 -0600

    <got my API key working, now just have use javascript to get in the webapp>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}")
+fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=0372eaa7cde1ce19de6c28dd0eb2454c')
+
+.then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  });
