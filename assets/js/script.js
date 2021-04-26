75746b74d867754a2022ea16dc92a45e18202dd3
CastroOlympias <blackcarrera@msn.com>
2021-04-25 18:23:08 -0600

    <cleaned up and tidied up the code and comments/notes>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,14 @@
 var submitBtn = document.querySelector('#submit-btn');
 
-
-// this conole logged a complete word "this" as test of my understanding
-var one = "th"
-var two = "i"
-var three = "s"
-var whole = one + two + three
-console.log(whole);
-//  this conole logged a complete word "this" as test of my understanding
-
 // Starts with an empty array
 var findCity = [];
 // console.log(localStorage.getItem("City"));
 
-// gets local storage string of historical city searches and puts them back into an array
+// gets local storage string of historical city searches and puts them back into an array, so storage isn't empty on refresh
 findCity = JSON.parse(localStorage.getItem('City')) || [];
 console.log(findCity);
 
-// This foor loop is to append only the last 10 items found in the array of the search history
-
+// This foor loop is to append only the last 10 items found in the array of the search history. Code is still beta
 // function searchHistory() {
 //     for (var i = findCity.length -1; i >= 0; i--) {
 //         console.log(findCity[i])
@@ -30,9 +20,8 @@ console.log(findCity);
 // }
 // searchHistory();
 
-
+// This creates a button from the search, working beta, would like to create button from local storage to prevent duplicate buttons
 function createBtn (searchValue) {
-    
     var history = document.querySelector('.history');
     var lI = document.createElement('button')
     lI.textContent = searchValue
@@ -41,19 +30,16 @@ function createBtn (searchValue) {
     history.append(lI)
 }
 
-
-// submit button begins the fuction to collect and apend the weather data
+// submit button begins the fuction to collect and apend the weather data to the webpage
 submitBtn.onclick = findWeather;
 function findWeather(event) {
-event.preventDefault();
+    event.preventDefault();
 
-
-    // Sets the searched cities as an arraay and stores into a variable for storage as a string of historical searches
+    // Sets the searched cities as an array and stores into a variable for storage as a string of historical searches
     var searchTerm = document.querySelector('#city-search').value;
-
     createBtn(searchTerm)
 
-   
+    // This changes user input to all lowercase, stores only lower case values in local storage and prevents duplicates values
     var lowerCase = searchTerm.toLowerCase()
     console.log(lowerCase);
     if (findCity.indexOf(lowerCase)=== -1) {
@@ -73,16 +59,17 @@ event.preventDefault();
     .then(function(response) {
         console.log(response);
 
-        // current days weather
+    // City, date and weather icon pus weather
         var currentCityEl = document.querySelector('#city')
         currentCityEl.textContent = response.name;
 
-        // Current Date plus formatting date
+        // Current Date plus formatting date, insertng to html
         var currentDate = document.querySelector('#current-date')
         var today = moment().format('(MM/DD/YY)');
         console.log(today)
         currentDate.textContent = today    
 
+        // weather, climate conditions inserting to html
         var cityCurTempEl = document.querySelector('#cur-temp')
         cityCurTempEl.textContent = "Current Temperature: " + response.main.temp + " °F ";
 
@@ -98,7 +85,7 @@ event.preventDefault();
         var cityWetEl = document.querySelector('#humid')
         cityWetEl.textContent = "Humidity: " + response.main.humidity + " % ";
 
-         // This appends the curent weather icon to the page
+         // This inserts the curent weather icon to the page
          var currentWeathericonPartOne = 'http://openweathermap.org/img/wn/';
          var currentWeathericonPartTwo = response.weather[0].icon;
          var currentWeathericonPartThree = '@2x.png';
@@ -125,32 +112,34 @@ event.preventDefault();
     .then(function(response) {
         console.log(response);
 
-        // plus 1 day formatting date
+    // Day 1 forecasting section
+        // plus 1 day formatting date, inserting into html
         var plusOneDate = document.querySelector('.day-one')
         var dayPlusOne = moment().add(1, 'd').format('MM/DD/YY');
         console.log(dayPlusOne)
         plusOneDate.textContent = dayPlusOne;
 
-        // Plus 1 day wearther forecast
+        // Plus 1 day wearther forecast, inserting into html
         var dayOneTempEl = document.querySelector('#day-one-temp')
-        var dayOneWindEl = document.querySelector('#day-one-wind')
-        var dayOneHumEl = document.querySelector('#day-one-hum')
-
         dayOneTempEl.textContent = "Temp: " + response.list[3].main.temp + " °F";
+
+        var dayOneWindEl = document.querySelector('#day-one-wind')
         dayOneWindEl.textContent = "Wind: " + response.list[3].wind.speed + " Mph";
+
+        var dayOneHumEl = document.querySelector('#day-one-hum')
         dayOneHumEl.textContent = "Humid: " + response.list[3].main.humidity + " %";
 
-        // This appends the plus 1 day weather icon to the page
+        // This inserts the plus 1 day weather icon to the page
         var plusOneWeatherIconPartOne = 'http://openweathermap.org/img/wn/';
         var plusOneWeatherIconPartTwo = response.list[3].weather[0].icon;
         var plusOneWeatherIconPartThree = '@2x.png';
         var plusOneWholeIcon = plusOneWeatherIconPartOne + plusOneWeatherIconPartTwo + plusOneWeatherIconPartThree;
         console.log(plusOneWholeIcon);
-
         var plusOneIcon = document.querySelector('#plus-one-weather-icon')
         plusOneIcon.setAttribute('src', plusOneWholeIcon);
 
-        // plus 2 day formatting date
+    // Day 2 forecasting section
+        // plus 2 day formatting date, inserting into html
         var plusTwoDate = document.querySelector('.day-two')
         var dayPlusTwo = moment().add(2, 'd').format('MM/DD/YY');
         console.log(dayPlusTwo)
@@ -158,83 +147,77 @@ event.preventDefault();
 
         // Plus 2 days wearther forecast
         var dayTwoTempEl = document.querySelector('#day-two-temp')
-        var dayTwoWindEl = document.querySelector('#day-two-wind')
-        var dayTwoHumEl = document.querySelector('#day-two-hum')
-
-       
         dayTwoTempEl.textContent = "Temp: " + response.list[11].main.temp + " °F";
+
+        var dayTwoWindEl = document.querySelector('#day-two-wind')
         dayTwoWindEl.textContent = "Wind: " + response.list[11].wind.speed + " Mph";
+
+        var dayTwoHumEl = document.querySelector('#day-two-hum')
         dayTwoHumEl.textContent = "Humid: " + response.list[11].main.humidity + " %";
 
-        // This appends the plus 2 day weather icon to the page
+        // This inserts the plus 2 day weather icon to the page
         var plusTwoWeatherIconPartOne = 'http://openweathermap.org/img/wn/';
         var plusTwoWeatherIconPartTwo = response.list[11].weather[0].icon;
         var plusTwoWeatherIconPartThree = '@2x.png';
         var plusTwoWholeIcon = plusTwoWeatherIconPartOne + plusTwoWeatherIconPartTwo + plusTwoWeatherIconPartThree;
         console.log(plusTwoWholeIcon);
-
         var plusTwoIcon = document.querySelector('#plus-two-weather-icon')
         plusTwoIcon.setAttribute('src', plusTwoWholeIcon);
 
-
+    // Day 3 forecasting section
         // plus 3 day formatting date
         var plusThreeDate = document.querySelector('.day-three')
         var dayPlusThree = moment().add(3, 'd').format('MM/DD/YY');
         console.log(dayPlusThree)
         plusThreeDate.textContent = dayPlusThree;
 
-
         // Plus 3 days weather forecast
-        
         var dayThreeTempEl = document.querySelector('#day-three-temp')
-        var dayThreeWindEl = document.querySelector('#day-three-wind')
-        var dayThreeHumEl = document.querySelector('#day-three-hum')
-
         dayThreeTempEl.textContent = "Temp: " + response.list[19].main.temp + " °F";
+
+        var dayThreeWindEl = document.querySelector('#day-three-wind')
         dayThreeWindEl.textContent = "Wind: " + response.list[19].wind.speed + " Mph";
+
+        var dayThreeHumEl = document.querySelector('#day-three-hum')
         dayThreeHumEl.textContent = "Humid: " + response.list[19].main.humidity + " %";
 
-        // This appends the plus 3 day weather icon to the page
+        // This inserts the plus 3 day weather icon to the page
         var plusThreeWeatherIconPartOne = 'http://openweathermap.org/img/wn/';
         var plusThreeWeatherIconPartTwo = response.list[19].weather[0].icon;
         var plusThreeWeatherIconPartThree = '@2x.png';
         var plusThreeWholeIcon = plusThreeWeatherIconPartOne + plusThreeWeatherIconPartTwo + plusThreeWeatherIconPartThree;
         console.log(plusThreeWholeIcon);
-
         var plusThreeIcon = document.querySelector('#plus-three-weather-icon')
         plusThreeIcon.setAttribute('src', plusThreeWholeIcon);
 
 
-
+    // Day 4 forecasting section
         // plus 4 day formatting date
         var plusFourDate = document.querySelector('.day-four')
         var dayPlusFour = moment().add(4, 'd').format('MM/DD/YY');
         console.log(dayPlusFour)
         plusFourDate.textContent = dayPlusFour;
 
-
         // Plus 4 days wearther forecast
-  
         var dayFourTempEl = document.querySelector('#day-four-temp')
-        var dayFourWindEl = document.querySelector('#day-four-wind')
-        var dayFourHumEl = document.querySelector('#day-four-hum')
-
- 
         dayFourTempEl.textContent = "Temp: " + response.list[27].main.temp + " °F";
+
+        var dayFourWindEl = document.querySelector('#day-four-wind')
         dayFourWindEl.textContent = "Wind: " + response.list[27].wind.speed + " Mph";
+
+        var dayFourHumEl = document.querySelector('#day-four-hum')
         dayFourHumEl.textContent = "Humid: " + response.list[27].main.humidity + " %";
 
-        // This appends the plus 4 day weather icon to the page
+        // This inserts the plus 4 day weather icon to the page
         var plusFourWeatherIconPartOne = 'http://openweathermap.org/img/wn/';
         var plusFourWeatherIconPartTwo = response.list[27].weather[0].icon;
         var plusFourWeatherIconPartThree = '@2x.png';
         var plusFourWholeIcon = plusFourWeatherIconPartOne + plusFourWeatherIconPartTwo + plusFourWeatherIconPartThree;
         console.log(plusFourWholeIcon);
-
         var plusFourIcon = document.querySelector('#plus-four-weather-icon')
         plusFourIcon.setAttribute('src', plusFourWholeIcon);
 
-
+    // Day 5 forecasting section
         // plus 5 day formatting date
         var plusFiveDate = document.querySelector('.day-five')
         var dayPlusFive = moment().add(5, 'd').format('MM/DD/YY');
@@ -242,28 +225,27 @@ event.preventDefault();
         plusFiveDate.textContent = dayPlusFive;
 
         // Plus 5 days wearther forecast
-        
         var dayFiveTempEl = document.querySelector('#day-five-temp')
-        var dayFiveWindEl = document.querySelector('#day-five-wind')
-        var dayFiveHumEl = document.querySelector('#day-five-hum')
-
-        
         dayFiveTempEl.textContent = "Temp: " + response.list[35].main.temp + " °F";
+
+        var dayFiveWindEl = document.querySelector('#day-five-wind')
         dayFiveWindEl.textContent = "Wind: " + response.list[35].wind.speed + " Mph";
+
+        var dayFiveHumEl = document.querySelector('#day-five-hum')
         dayFiveHumEl.textContent = "Humid: " + response.list[35].main.humidity + " %";
 
-        // This appends the plus 5 day weather icon to the page
+        // This inserts the plus 5 day weather icon to the page
         var plusFiveWeatherIconPartOne = 'http://openweathermap.org/img/wn/';
         var plusFiveWeatherIconPartTwo = response.list[35].weather[0].icon;
         var plusFiveWeatherIconPartThree = '@2x.png';
         var plusFiveWholeIcon = plusFiveWeatherIconPartOne + plusFiveWeatherIconPartTwo + plusFiveWeatherIconPartThree;
         console.log(plusFiveWholeIcon);
-
         var plusFiveIcon = document.querySelector('#plus-five-weather-icon')
         plusFiveIcon.setAttribute('src', plusFiveWholeIcon);
     })
 }
+
+
 // for looping through the array of previously searched cities
 // for (var i = 0; i < 10; i++);
 // console.log(findCity);
-
